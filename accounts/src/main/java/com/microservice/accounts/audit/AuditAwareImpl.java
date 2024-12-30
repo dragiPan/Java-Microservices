f6c92a689626294aacae3007796c3e9e1b6a2a30
Dragi <dtm.pantovic@gmail.com>
2024-12-30 16:29:14 +0100

    Added Eureka, changed docker compose, added mysql databases for apps

--- accounts/src/main/java/com/microservice/accounts/audit/AuditAwareImpl.java
+++ accounts/src/main/java/com/microservice/accounts/audit/AuditAwareImpl.java
@@ -15,6 +15,6 @@ public class AuditAwareImpl implements AuditorAware<String> {
      */
     @Override
     public Optional<String> getCurrentAuditor() {
-        return Optional.of("LOANS_MS");
+        return Optional.of("ACCOUNTS_MS");
     }
 }
